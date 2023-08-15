d04023961cf48044bdcce897fdd4b90c235de256
Reda Baydoun <reda.baydoun@gmail.com>
2023-08-15 10:56:13 -0400

    feat: add demo golang influxdb code

--- main.go
+++ main.go
@@ -5,8 +5,17 @@ import (
 	"os"
 	"flag"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
+
+	"time"
+	"log"
+	"context"
+	"strconv"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api/write"
 )
 
+
+// MQTT STUFFZ
 var h MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
 	fmt.Println("*** Received Message ***")
 	fmt.Printf("  TOPIC: %s\n", msg.Topic())
@@ -25,7 +34,17 @@ func initFlags() {
 	flag.Parse()
 }
 
-func main() {
+func produce(client MQTT.Client) {
+	fmt.Println("[+] Producing stuff")
+	for i := 0; i < 5; i++ {
+		msg := fmt.Sprintf("this is msg #%d", i)
+		token := client.Publish("gosquitto/test", 0, false, msg)
+		token.Wait()
+	}
+	fmt.Println("[+] Done...")
+}
+
+func testMQTT() {
 	fmt.Println("[+] Parsing command line flags.")
 	initFlags()
 
@@ -60,12 +79,88 @@ func main() {
 	c.Disconnect(250)
 }
 
-func produce(client MQTT.Client) {
-	fmt.Println("[+] Producing stuff")
-	for i := 0; i < 5; i++ {
-		msg := fmt.Sprintf("this is msg #%d", i)
-		token := client.Publish("gosquitto/test", 0, false, msg)
-		token.Wait()
+
+
+func main() {
+
+	// Initialize a client to InfluxDB
+	token := os.Getenv("INFLUXDB_TOKEN")
+	url := "http://thoth.local:8086"
+	client := influxdb2.NewClient(url, token)
+
+
+	org := "THG"
+	bucket := "Test"
+	writeAPI := client.WriteAPIBlocking(org, bucket)
+
+
+	writeNewData := false
+
+	if writeNewData {
+		fmt.Println("[+] Starting write to DB")
+		for sensorId := 0; sensorId < 5; sensorId++ {
+			fmt.Printf("  [+] Writing data for sensor %d\n", sensorId)
+			for value := 0; value < 5; value++ {
+				tags := map[string]string {
+					"sensor": strconv.Itoa(sensorId),
+				}
+
+				fields := map[string]interface{} {
+					"field1": value,
+				}
+
+				point := write.NewPoint("measurement1", tags, fields, time.Now())
+				time.Sleep(1 * time.Second)
+
+				if err := writeAPI.WritePoint(context.Background(), point); err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+		fmt.Println("[+] Done writing...")
 	}
-	fmt.Println("[+] Done...")
+
+	
+	fmt.Println("[+] Waiting...")
+	time.Sleep(5 * time.Second)
+
+	queryAPI := client.QueryAPI(org)
+	query := `from(bucket: "Test")
+							|> range(start: -10m)
+						  |> filter(fn: (r) => r._measurement == "measurement1")`
+
+	fmt.Println("[+] Starting to query the DB (single)")
+	results, err := queryAPI.Query(context.Background(), query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for results.Next() {
+		fmt.Println(results.Record())
+	}
+	if err := results.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("[+] Done querying the DB (single)")
+
+
+
+	fmt.Println("[+] Waiting...")
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("[+] Starting to query the DB (aggregate)")
+	queryAgg := `from(bucket: "Test")
+							|> range(start: -20m)
+							|> filter(fn: (r) => r._measurement == "measurement1")
+							|> mean()`
+	results, err = queryAPI.Query(context.Background(), queryAgg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for results.Next() {
+		fmt.Println(results.Record())
+	}
+	if err := results.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("[+] Done querying the DB (aggregate)")
 }
