1b61bed9976f06c82fe98d4318b955c3ae80c978
Reda Baydoun <reda.baydoun@gmail.com>
2023-10-15 14:32:59 -0400

    feat: revert removal of main.go

--- thgsink/config/sink.sh
+++ thgsink/config/sink.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export THG_INFLUXDB_WRITE_TOKEN=""
-export THG_INFLUXDB_URL="http://:8086"
+export THG_INFLUXDB_WRITE_TOKEN="mJtMpK-uIa55uH4h1qkv-5QBWDU1BYhypkGqjDvoBT4JJHhtyyk7KJQMBwDsufTIzR69Qd1rFjEMT62V_ds_ug=="
+export THG_INFLUXDB_URL="http://thoth.local:8086"
 export THG_INFLUXDB_ORGANIZATION="THG"
 export THG_INFLUXDB_BUCKET="Test"
 
-export THG_MQTT_HOSTNAME=""
+export THG_MQTT_HOSTNAME="thoth.local"
 export THG_MQTT_TOPIC="thg/test-topic"
 export THG_MQTT_CLIENT_NAME="thg-test-client"
-export THG_MQTT_USER=""
-export THG_MQTT_PASS=""
+export THG_MQTT_USER="mqtt"
+export THG_MQTT_PASS="mqtt"
